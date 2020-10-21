27681e326b4e38fb7c31c1f3a217dd38cf8b8cba
Muh-Hasan <muh.hasan.git@gmail.com>
2020-10-21 20:02:41 +0500

    fetching data completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,35 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  console.log(data);
+return <div>{data.allContentfulBlogPosts.edges[0].node.title}</div>
 }
+
+export const qurey = graphql`
+  query {
+    allContentfulBlogPosts {
+      edges {
+        node {
+          publicationDate
+          userName
+          updatedAt
+          userImage {
+            file {
+              url
+            }
+          }
+          title
+          image {
+            file {
+              url
+            }
+          }
+          content {
+            content
+          }
+        }
+      }
+    }
+  }
+`
